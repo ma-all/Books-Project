702dd00b6c1f8463b6d83ccaf1f02c6b69a5f26b
ma-all <marwaalbaloushi21@gmail.com>
2026-07-16 23:36:08 +0300

    created the book schema

---
+++ models/book.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    },
+    pages: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    readingStatus: {
+        type: String,
+        enum: ['Done Reading', 'Currently Reading', 'To Be Read'],
+        required: true,
+    },
+})
+const Book = mongoose.model('Book', bookSchema)
+module.exports = Book
