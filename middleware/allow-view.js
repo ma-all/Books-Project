3e5798dcf7c9fb1b5d1397c6b42e0077dc04177e
ma-all <marwaalbaloushi21@gmail.com>
2026-07-19 10:08:05 +0300

    wrote allow-view middleware

---
+++ middleware/allow-view.js
@@ -0,0 +1,9 @@
+const allowViewing = (req, res, next) => {
+    if(req.session.user){
+        res.locals.user = req.session.user
+    } else {
+        res.locals.user = null
+    }
+    next()
+    module.exports = allowViewing
+}
